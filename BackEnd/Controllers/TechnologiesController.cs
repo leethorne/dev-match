883f386e8f73f5df7d18d1b892039af51dafb6d0
Felicia Thorne <feliciathorne@Felicias-MacBook-Pro.local>
2017-12-21 15:20:34 -0800

    added technology controller/fully functional

--- BackEnd/Controllers/TechnologiesController.cs
+++ BackEnd/Controllers/TechnologiesController.cs
@@ -9,36 +9,109 @@ namespace BackEnd.Controllers
     [Route("api/technologies")]
     public class TechnologiesController : Controller
     {
+        private readonly DevMatchContext _context;
+
+        public TechnologiesController(DevMatchContext context)
+        {
+            _context = context;
+
+            if (_context.Technologies.Count() == 0)
+            {
+                _context.Technologies.Add(new Technology() { Id = 1, Name = "Python" });
+                _context.Technologies.Add(new Technology() { Id = 2, Name = "Ajax" });
+                _context.Technologies.Add(new Technology() { Id = 3, Name = "jQuery" });
+                _context.Technologies.Add(new Technology() { Id = 4, Name = "Django" });
+                _context.Technologies.Add(new Technology() { Id = 5, Name = "MySQL" });
+                _context.Technologies.Add(new Technology() { Id = 6, Name = "MongoDB" });
+                _context.Technologies.Add(new Technology() { Id = 7, Name = "Angular" });
+                _context.Technologies.Add(new Technology() { Id = 8, Name = "React" });
+                _context.Technologies.Add(new Technology() { Id = 9, Name = "Node.js" });
+                _context.Technologies.Add(new Technology() { Id = 10, Name = "Swift" });
+                _context.Technologies.Add(new Technology() { Id = 11, Name = "Xcode" });
+                _context.Technologies.Add(new Technology() { Id = 12, Name = "C#" });
+                _context.Technologies.Add(new Technology() { Id = 13, Name = ".NET Core" });
+                _context.Technologies.Add(new Technology() { Id = 14, Name = "NancyFX" });
+                _context.Technologies.Add(new Technology() { Id = 15, Name = "ASP.NET Core" });
+                _context.Technologies.Add(new Technology() { Id = 16, Name = "SQL Server" });
+                _context.Technologies.Add(new Technology() { Id = 17, Name = "Dapper" });
+                _context.Technologies.Add(new Technology() { Id = 18, Name = "Entity Framework Core" });
+                _context.Technologies.Add(new Technology() { Id = 19, Name = "Azure" });
+                _context.Technologies.Add(new Technology() { Id = 20, Name = "AWS" });
+                _context.Technologies.Add(new Technology() { Id = 21, Name = "PHP" });
+                _context.Technologies.Add(new Technology() { Id = 22, Name = "Java" });
+                _context.Technologies.Add(new Technology() { Id = 23, Name = "SpringMVC" });
+                _context.Technologies.Add(new Technology() { Id = 24, Name = "Ruby" });
+                _context.Technologies.Add(new Technology() { Id = 25, Name = "C / C++" });
+                _context.Technologies.Add(new Technology() { Id = 26, Name = "Javascript" });
+                _context.Technologies.Add(new Technology() { Id = 27, Name = "Bootstrap" });
+                _context.Technologies.Add(new Technology() { Id = 28, Name = "Objective-C" });
+                _context.Technologies.Add(new Technology() { Id = 29, Name = "HTML" });
+                _context.Technologies.Add(new Technology() { Id = 30, Name = "CSS" });
+                _context.SaveChanges();
+            }
+        }
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<Technology> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _context.Technologies.ToList();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Technology Get(int id)
         {
-            return "value";
+            foreach(Technology t in _context.Technologies)
+            {
+                if (t.Id == id)
+                {
+                    return t;
+                }
+            }
+            return null;
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public Technology Post([FromBody]Technology t)
         {
+            _context.Technologies.Add(t);
+            _context.SaveChanges();
+            return t;
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public Technology Put(int id, [FromBody]Technology tech)
         {
+            foreach (Technology t in _context.Technologies)
+            {
+                if (t.Id == id)
+                {
+                    _context.Technologies.Remove(t);
+                    _context.SaveChanges();
+                    _context.Technologies.Add(tech);
+                    _context.SaveChanges();
+                    return tech;
+
+                }
+            }
+            return null;
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public string Delete(int id)
         {
+            foreach(Technology t in _context.Technologies)
+            {
+                if (t.Id == id)
+                {
+                    _context.Technologies.Remove(t);
+                    _context.SaveChanges();
+                    return "Deleted Technology";
+                }
+            } return "Technology not found";
         }
     }
 }
