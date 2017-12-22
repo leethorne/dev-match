b03061c87333dba955c3dad91801db8c5527c525
Felicia Thorne <feliciathorne@Felicias-MacBook-Pro.local>
2017-12-21 16:10:01 -0800

    created get,getbyId,add,delete user functions for user controller and service.

--- FrontEnd/controllers/userController.js
+++ FrontEnd/controllers/userController.js
@@ -1,3 +1,65 @@
-app.controller("userController", function($scope, $stateParams, userService) {
+app.controller("userController", function($scope, $state, $stateParams, userService) {
   
+    userService.getUsers()
+    .then(function(response){
+        console.log(response.data)
+        //do something with route data to display. set equal to $scope.something to ng-repeat
+    }, function(error){
+        console.log(error);
+        //do something else here to alert user of a fail
+    })
+
+    if($stateParams.id == null || $stateParams.id == undefined || $stateParams.id == "") {
+        $scope.submitButton = true;
+        $scope.heading = "Create Your DevMatch Profile!"
+
+        userService.getUserById($stateParams.id, function (user) {
+            $scope.user = user;
+            console.log($scope.user);
+        })
+    } else {
+        $scope.submitButton = false;
+        $scope.heading = "Update Your DevMatch Profile!";
+
+        userService.getUserById($stateParams.id, function(user) {
+            $scope.user = user;
+            console.log($scope.user);
+        })
+    }
+
+    $scope.addUser = function() {
+        userService.addUser($scope.user)
+        .then(function(response) {
+            console.log(response.data);
+            //do something with route data to display. set equal to $scope.something to ng-repeat
+            //$state.go to profile page or dev map?
+        }, function(error) {
+            console.log(error)
+            //do something here to display error msg
+        })
+    }
+
+    $scope.updateUser = function() {
+        userService.updateUser($stateParams.id, $scope.user)
+        .then(function (response) {
+            console.log(response.data);
+            //once we get results set the $scope.user to response.data
+            //state.go to profile/"my account" page?
+        }, function(error) {
+            console.log(error)
+            //insert error message here for users
+        })
+    }
+
+    $scope.deleteUser = function() {
+        userService.deleteUser($stateParams.id)
+        .then(function(response) {
+            $state.go("home");
+        }, function(error) {
+            console.log(error)
+            //set error message here for users to see
+        })
+    }
+
 }); 
+
--- FrontEnd/services/userService.js
+++ FrontEnd/services/userService.js
@@ -1,3 +1,34 @@
 app.service("userService", function($http, $state) {
+    
+    this.getUsers = function () {
+        return $http.get("http://localhost:50771/api/users");
+    }
 
+    this.getUserById = function(id, cb) {
+        if(id == null || id == undefined || id == "") {
+            var user = {};
+            cb(user);
+        } else {
+            $http.get("http://localhost:50771/api/users" + id)
+            .then (function(response) {
+                console.log(response)
+                cb(response.data)
+            }, function(error) {
+                console.log(error)
+                //do something else here to alert user of a fail
+            })
+        }
+    }
+
+    this.addUser = function(user) {
+        return $http.post("http://localhost:50771/api/users" + user)
+    }
+
+    this.updateUser = function(id, user) {
+        return $http.put("http://localhost:50771/api/users" + id, user)
+    }
+
+    this.deleteUser = function(id) {
+        return $http.delete("http://localhost:50771/api/users" + id);
+    }
 });
