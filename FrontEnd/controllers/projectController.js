3985564bfe99c13ca36693b90c7c1d4fde4e12ac
Felicia Thorne <feliciathorne@Felicias-MacBook-Pro.local>
2017-12-21 16:46:55 -0800

    created get, getbyID, add, delete, update functions in FE project controller

--- FrontEnd/controllers/projectController.js
+++ FrontEnd/controllers/projectController.js
@@ -1,3 +1,55 @@
-app.controller("projectController", function($scope, $stateParams, projectService) {
+app.controller("projectController", function($scope, $state,$stateParams, projectService) {
 
+    projectService.getProjects()
+    .then(function(response) {
+        console.log(response);
+        //set $scope.projects to something here once we can get data
+    }, function(error) {
+        console.log(error);
+        //handle error messages here to the user
+    })
+
+    if($stateParams.id == null || $stateParams.id == undefined || $stateParams.id == "") {
+        projectService.getProjectById($stateParams.id, function(project) {
+            $scope.project = project;
+            console.log($scope.project);
+        })
+    } else {
+        projectService.getProjectById($stateParams.id, function(project) {
+            $scope.project = project;
+            console.log($scope.project);
+        })
+    }
+
+    $scope.addProject = function() {
+        projectService.addProject($scope.project)
+        .then(function(reponse) {
+            $scope.project = response.data;
+            console.log($scope.project)
+            //$state.go to porject wall?
+        }, function(error) {
+            console.log(error)
+        })
+    }
+
+    $scope.updateProject = function() {
+        projectService.updateProject($stateParams.id, $scope.project)
+        .then (function(response) {
+            console.log(response)
+            //set $scope.project to reponse data? 
+            //state.go to project page? 
+        }, function(error) {
+            console.log(error)
+        })
+    }
+
+    $scope.deleteProject = function() {
+        projectService.deleteProject($stateParams.id)
+        .then(function(response) {
+            $state.go("projects")
+        }, function(error) {
+            console.log(error);
+            //make an error message for the user 
+        })
+    }
 }); 
--- FrontEnd/services/projectService.js
+++ FrontEnd/services/projectService.js
@@ -1,3 +1,33 @@
 app.service("projectService", function($http, $state) {
   
+    this.getProjects = function() {
+        return $http.get("http://localhost:5000/api/projects")
+    }
+
+    this.getProjectById = function(id, cb) {
+        if (id == null || id == undefined || id == "") {
+            var project = {};
+            cb(project)
+        } else {
+            $http.get("http://localhost:5000/api/projects" + id)
+            .then (function(repsonse) {
+                console.log(response)
+                cb(response.data)
+            }, function(error) {
+                console.log(error)
+            })
+        }
+    }
+
+    this.addProject = function(project) {
+        return $http.post("http://localhost:5000/api/projects" + project)
+    }
+
+    this.updateProject = function(id, project) {
+        return $http.put("http://localhost:5000/api/projects" + id, project)
+    }
+
+    this.deleteProject = function(id) {
+        return $http.delete("http://localhost:5000/api/projects" + id)
+    }
 });
