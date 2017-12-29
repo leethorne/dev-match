878371b7cc52308927e97028c51001686b82c27d
William Pruden <williamprudeniv@gmail.com>
2017-12-28 21:26:35 -0800

    minor formating fixes and code cleanup

--- FrontEnd/controllers/userController.js
+++ FrontEnd/controllers/userController.js
@@ -67,55 +67,48 @@ app.controller("userController", function ($scope, $state, $stateParams, userSer
     }
 
     $scope.addUser = function () {
-        console.log("user before function: ", $scope.user)
-        console.log("image: ", $scope.user.image)
+      console.log("user before function: ", $scope.user)
+      console.log("image: ", $scope.user.image)
 
-        userService.addUser($scope.user)
+      userService.addUser($scope.user)
+        .then(function (response) {
+          // $scope.user = response.data;
+          console.log("userrrr: ", response.data)
+          console.log("image: ", response.data.image)
+
+          //ADDING TECH SKILLS TO USER
+          userService.updateUserTech(response.data.id, $scope.techName)
             .then(function (response) {
-                // $scope.user = response.data;
-                console.log("userrrr: ", response.data)
-                console.log("image: ", response.data.image)
-                
-                //ADDING TECH SKILLS TO USER 
-                    userService.updateUserTech(response.data.id, $scope.techName)
-                        .then(function (response) {
-                            console.log(response)
-                        }, function (error) {
-                            console.log(error);
-                            //do something here to alert user of fail 
-                        })
-                
-                $state.go("user", {
-                    id: $scope.user.id
-                });
-            }, function (error) {
-                console.log("you have an error: ", error)
-                //do something here to display error msg
+              console.log(response)
+            },
+            function (error) {
+              console.log(error);
             })
+
+            $state.go("user", { id: $scope.user.id });
+        }, function (error) {
+            console.log("you have an error: ", error)
+        })
     }
 
     $scope.updateUser = function () {
-        userService.updateUser($stateParams.id, $scope.user)
-            .then(function (response) {
-                console.log(response.data);
-                $scope.user = response.data;
-                $state.go("user", {
-                    id: $scope.user.id
-                });
-            }, function (error) {
-                console.log(error)
-                //insert error message here for users
-            })
+      userService.updateUser($stateParams.id, $scope.user)
+        .then(function (response) {
+            console.log(response.data);
+            $scope.user = response.data;
+            $state.go("user", { id: $scope.user.id });
+        }, function (error) {
+            console.log(error)
+        })
     }
 
     $scope.deleteUser = function () {
-        userService.deleteUser($stateParams.id)
-            .then(function (response) {
-                $state.go("home");
-            }, function (error) {
-                console.log(error)
-                //set error message here for users to see
-            })
+      userService.deleteUser($stateParams.id)
+        .then(function (response) {
+          $state.go("home");
+        }, function (error) {
+          console.log(error)
+        })
     }
 
     $scope.addProject = function () {
@@ -123,97 +116,96 @@ app.controller("userController", function ($scope, $state, $stateParams, userSer
 
         // Looping through Seeking Skills array and setting isSeeking = True
         for (var i = 0; i < $scope.project.seekingSkills.length; i++) {
-            $scope.project.seekingSkills[i].isSeeking = true;
-            skillsArray.push($scope.project.seekingSkills[i])
+          $scope.project.seekingSkills[i].isSeeking = true;
+          skillsArray.push($scope.project.seekingSkills[i])
         }
 
         var added = null;
 
         // Looping through Using Skills array and setting isUsing = True
         for (var i = 0; i < $scope.project.usingSkills.length; i++) {
-            added = false
-            for (var j = 0; j < skillsArray.length; j++) {
-                if ($scope.project.usingSkills[i].name == skillsArray[j].name) {
-                    skillsArray[j].isUsing = true;
-                    added = true;
-                }
+          added = false
+          for (var j = 0; j < skillsArray.length; j++) {
+            if ($scope.project.usingSkills[i].name == skillsArray[j].name) {
+              skillsArray[j].isUsing = true;
+              added = true;
             }
-            if (!added) {
-                $scope.project.usingSkills[i].isUsing = true;
-                skillsArray.push($scope.project.usingSkills[i])
-            }
+          }
+          if (!added) {
+            $scope.project.usingSkills[i].isUsing = true;
+            skillsArray.push($scope.project.usingSkills[i])
+          }
         }
 
         console.log($scope.project)
         console.log(skillsArray);
 
         projectService.addProject($scope.project)
-            .then(function (response) {
-                // $scope.project = response.data;
-                console.log("ADD proj SUCCESS: ", response)
+          .then(function (response) {
+            // $scope.project = response.data;
+            console.log("ADD proj SUCCESS: ", response)
 
-                //adding user to project
-                userService.updateUserProj($stateParams.id, response.data.id)
-                    .then(function (response) {
-                        console.log("USER ADDED: ", response)
-                    }, function (error) {
-                        console.log("error adding use to proj: ", error);
-                        //do something here to alert user of fail 
-                    })
+            //adding user to project
+            userService.updateUserProj($stateParams.id, response.data.id)
+              .then(function (response) {
+                console.log("USER ADDED: ", response)
+              }, function (error) {
+                console.log("error adding use to proj: ", error);
+                //do something here to alert user of fail
+              })
 
 
-                //ADDING TECH TO PROJECT
-                skillsArray.forEach(function (element) {
-                    projectService.updateProjTech(response.data.id, element.name, element.isSeeking, element.isUsing)
-                        .then(function (response) {
-                            console.log("ADD TECH TO PROJ - SUCCESSFUL: ", response)
-                        }, function (error) {
-                            console.log("error updating seeking tech: ", error)
-                            //alert here 
-                        })
-                });
+            //ADDING TECH TO PROJECT
+            skillsArray.forEach(function (element) {
+              projectService.updateProjTech(response.data.id, element.name, element.isSeeking, element.isUsing)
+                .then(function (response) {
+                  console.log("ADD TECH TO PROJ - SUCCESSFUL: ", response)
+                }, function (error) {
+                  console.log("error updating seeking tech: ", error)
+                })
+            });
 
-                $state.go('user', {}, { reload: 'user'})
-            }, function (error) {
-                console.log("error adding proj: ", error)
-                //make error message for user if failed
-            })
+            $state.go('user', {}, { reload: 'user'})
+          },
+          function (error) {
+            console.log("error adding proj: ", error)
+          })
     }
 
     $scope.updateUserProj = function () {
-        userService.updateUserProj($stateParams.id, $scope.projId)
-            .then(function (response) {
-                console.log(response)
-            }, function (error) {
-                console.log(error);
-                //do something here to alert user of fail 
-            })
-    }
-
-    $scope.updateUserTech = function () {
-        userService.updateUserTech($stateParams.id, $scope.techName)
-            .then(function (response) {
-                console.log(response)
-            }, function (error) {
-                console.log(error);
-                //do something here to alert user of fail 
-            })
-    }
-
-    $scope.login = function () {
-        userService.login($scope.user, function (message) {
-            $scope.errorMessage = message;
+      userService.updateUserProj($stateParams.id, $scope.projId)
+        .then(function (response) {
+          console.log(response)
+        }, function (error) {
+          console.log(error);
+          //do something here to alert user of fail
         })
     }
 
+    $scope.updateUserTech = function () {
+      userService.updateUserTech($stateParams.id, $scope.techName)
+        .then(function (response) {
+          console.log(response)
+        }, function (error) {
+          console.log(error);
+          //do something here to alert user of fail
+        })
+    }
+
+    $scope.login = function () {
+      userService.login($scope.user, function (message) {
+        $scope.errorMessage = message;
+      })
+    }
+
     $scope.logout = function () {
-        userService.logout($scope.currentUser);
-        $state.go("home");
+      userService.logout($scope.currentUser);
+      $state.go("home");
     }
 
     // user page --> user form
     $scope.editUser = function () {
-        userService.editUser($stateParams.id)
+      userService.editUser($stateParams.id)
     }
 
     // collapse login form
@@ -245,7 +237,7 @@ app.controller("userController", function ($scope, $state, $stateParams, userSer
         });
     });
 
-    // input fields 
+    // input fields
     (function() {
         // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
         if (!String.prototype.trim) {
@@ -283,20 +275,20 @@ app.controller("userController", function ($scope, $state, $stateParams, userSer
 
      //API functionality -- get map from the address of the users
      $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD7RhAqkBYU5QS2Z75F-0jujx2e8bKJ-n4";
-     
+
          NgMap.getMap().then(function (map) {
              console.log(map.getCenter());
              console.log('markers', map.markers);
          });
-     
+
          //default zoom and center for usa on page load
          $scope.MapCenter = "38.457791, -99.641980"
          $scope.MapZoom = 5;
-     
-         //enact click events to trigger zoom over city 
+
+         //enact click events to trigger zoom over city
          $scope.zoomCitySearch = function () {
              cityKey = $scope.selectedCity;
-     
+
              switch (cityKey) {
                  case 'Long Beach':
                      $scope.MapCenter = "33.757120, -118.126273";
@@ -311,7 +303,7 @@ app.controller("userController", function ($scope, $state, $stateParams, userSer
                      $scope.MapZoom = 5;
              }
         }
-       
+
     // $scope.postImage = function() {
     //     $scope.image = $scope.scope.uploadme.substr(22);
     //     // console.log($scope.image);
@@ -323,5 +315,5 @@ app.controller("userController", function ($scope, $state, $stateParams, userSer
     //             console.log(error);
     //         })
     // }
-    
+
 });
--- FrontEnd/services/projectService.js
+++ FrontEnd/services/projectService.js
@@ -1,48 +1,50 @@
 app.service("projectService", function($http, $state) {
 
     this.getProjects = function() {
-        return $http.get(serverLink + "projects")
+      return $http.get(serverLink + "projects")
     }
 
     this.getProjectById = function(id, cb) {
         if (id == null || id == undefined || id == "") {
-            var project = {
-                seekingSkills: [],
-                usingSkills: [],
-                projectName: "",
-                description: "",
-                status: "",
-                desiredTeamSize: null
-            };
-            cb(project)
-        } else {
-            $http.get(serverLink + "projects/" + id)
+          var project = {
+              seekingSkills: [],
+              usingSkills: [],
+              projectName: "",
+              description: "",
+              status: "",
+              desiredTeamSize: null
+          };
+          cb(project)
+        }
+        else {
+          $http.get(serverLink + "projects/" + id)
             .then (function(response) {
-                console.log(response)
-                cb(response.data)
-            }, function(error) {
-                console.log(error)
+              console.log(response)
+              cb(response.data)
+            },
+            function(error) {
+              console.log(error)
             })
         }
     }
 
     this.addProject = function(project) {
-        return $http.post(serverLink + "projects/", project)
+      return $http.post(serverLink + "projects/", project)
     }
 
     this.updateProject = function(id, project) {
-        return $http.put(serverLink + "projects/" + id, project)
+      return $http.put(serverLink + "projects/" + id, project)
     }
 
     this.deleteProject = function(id) {
-        return $http.delete(serverLink + "projects/" + id)
+      return $http.delete(serverLink + "projects/" + id)
     }
 
     this.updateProjTech = function(projectId, techName, isSeeking, isUsing) {
-        return $http.put(serverLink + "projects/" + projectId + "/addtechnology?techName=" + techName + "&isSeeking=" + isSeeking + "&isUsing=" + isUsing)
+      return $http.put(serverLink + "projects/" + projectId + "/addtechnology?techName=" + techName + "&isSeeking=" + isSeeking + "&isUsing=" + isUsing)
     }
 
     this.getNews = function() {
-        return $http.get("https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=8b44e7a550f1436bbd9a0961cb9c838c")
+      return $http.get("https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=8b44e7a550f1436bbd9a0961cb9c838c")
     }
 });
